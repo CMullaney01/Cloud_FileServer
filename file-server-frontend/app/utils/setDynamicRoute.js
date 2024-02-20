ee5bd5bada1e7c6ffa3ecd0983aed79ff9a49219
CMullaney01 <conormullreid@live.com>
2024-02-20 16:00:03 +0000

    Changed Global text Color

--- file-server-frontend/app/utils/setDynamicRoute.js
+++ file-server-frontend/app/utils/setDynamicRoute.js
@@ -3,6 +3,8 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
+// Might not be needed in later versions of next.js
+// stops cache rendering in case a user becomes unauthenticated
 export function SetDynamicRoute() {
   const router = useRouter();
 
